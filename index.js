2480d5748f1523325eae36399201f54f2322cc77
paskkal.io <pascalobiora73@gmail.com>
2024-10-12 09:23:23 +0100

    FEAT: Remove Flash of Unstyled Content

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ let tl = gsap.timeline({
   },
 });
 
-tl.from("#demo", { ease: "linear" })
-  .from("h1", { xPercent: 80, duration: 1 })
-  .from("h2", { x: -80, duration: 1 }, "<")
-  .from("p", { y: 30 }, "-=0.2")
-  .from("button", { y: 50 }, "-=0.4")
-  .from(
-    "#items > g",
-    {
-      scale: 0,
-      stagger: 0.1,
-      transformOrigin: "50% 50%",
-    },
-    "-=0.5"
-  );
+let init = () => {
+  tl.from("#demo", { ease: "linear", autoAlpha: 0 })
+    .from("h1", { xPercent: 80, duration: 1 })
+    .from("h2", { x: -80, duration: 1 }, "<")
+    .from("p", { y: 30 }, "-=0.2")
+    .from("button", { y: 50 }, "-=0.4")
+    .from(
+      "#items > g",
+      {
+        scale: 0,
+        stagger: 0.1,
+        transformOrigin: "50% 50%",
+      },
+      "-=0.5"
+    );
+};
+
+window.addEventListener("load", function (event) {
+  init();
+});
