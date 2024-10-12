9f2bcaeaa8760f5d444d9b3c8754ed8b9614d8b7
paskkal.io <pascalobiora73@gmail.com>
2024-10-12 09:08:47 +0100

    tweak timing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// GSDevTools.create();/
 let tl = gsap.timeline({
   defaults: {
     opacity: 0,
@@ -6,12 +7,16 @@ let tl = gsap.timeline({
 });
 
 tl.from("#demo", { ease: "linear" })
-  .from("h1", { xPercent: 80 })
-  .from("h2", { x: -80 })
-  .from("p", { y: 30 })
-  .from("button", { y: 30 })
-  .from("#items > g", {
-    scale: 0,
-    stagger: 0.1,
-    transformOrigin: "50% 50%",
-  });
+  .from("h1", { xPercent: 80, duration: 1 })
+  .from("h2", { x: -80, duration: 1 }, "<")
+  .from("p", { y: 30 }, "-=0.2")
+  .from("button", { y: 50 }, "-=0.4")
+  .from(
+    "#items > g",
+    {
+      scale: 0,
+      stagger: 0.1,
+      transformOrigin: "50% 50%",
+    },
+    "-=0.5"
+  );
